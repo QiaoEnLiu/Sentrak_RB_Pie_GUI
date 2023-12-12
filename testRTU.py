2d5a145c9b8e103247c7c8889c9e79bc9dc264f4
QiaoEnLiu <137969620+QiaoEnLiu@users.noreply.github.com>
2023-12-12 17:15:47 +0800

    20231212 1720

--- testRTU.py
+++ testRTU.py
@@ -4,7 +4,7 @@
 #此程式碼為測試模擬RTU數據取得
 
 try:
-    import minimalmodbus, traceback
+    import minimalmodbus, traceback, serial
 
     from PyQt5.QtCore import Qt
     from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QScrollArea
@@ -62,4 +62,24 @@ class testRTU_Frame(QWidget):
         main_layout.addLayout(self.testRTU_layout)
 
         self.RTU_Info_label.setText('RTU測試數據')
+
+        # 設置串列通訊
+        ser = serial.Serial('COM3', 19200, timeout=1)  # 替換 'COM3' 為你的串列埠，9600 是波特率
+
+        # 設置Modbus裝置
+        instrument = minimalmodbus.Instrument(ser.port, 1)  # 1 是裝置地址
+
+        try:
+            # 讀取保持寄存器中的數據（根據你的模擬軟體設定）
+            value = instrument.read_register(0, functioncode=3)  # 0 是寄存器地址，3 是功能碼（根據模擬軟體的設定）
+
+            # 處理讀取到的數據
+            print(f'Read value: {value}')
+
+        except Exception as e:
+            print(f'Error: {e}')
+
+        finally:
+            # 關閉串列通訊
+            ser.close()
         
